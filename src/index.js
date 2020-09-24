dcb9ab7b81cf827d3764f5c6406f3d95561a44c8
Andrii Krychkivskyi <kruchandriy@gmail.com>
2020-09-24 15:37:25 +0300

    Homework-12 ready

--- src/index.js
+++ src/index.js
@@ -1,119 +1,38 @@
 import './styles.css';
-import listTemplate from './templates/list.hbs';
-import conntryTemplate from './templates/country.hbs';
-
-import '@pnotify/core/dist/BrightTheme.css';
-import '@pnotify/core/dist/Material.css';
-import '@pnotify/core/dist/PNotify.css';
-// import 'material-design-icons/iconfont/material-icons.css';
-import { data } from 'autoprefixer';
-
-import { alert, notice, info, success, error } from '@pnotify/core';
-
-// import { notice, defaultModules } from '@pnotify/core';
-import * as PNotifyDesktop from '@pnotify/desktop';
-import { defaults } from '@pnotify/core';
-
-import fetchCountries from './fetch-countries';
-
-PNotifyDesktop.defaults = {
-  fallback: false,
-};
-
-// console.log(PNotifyDesktop);
-
-//   error({
-//     text: "I'm an error message.",
-//     delay: 180000,
-//     width: '360px',
-//     maxTextHeight: '150px',
-//   });
-//   console.log('Error', erro);
-// });
-
-// const myNotice = notice({
-//   text: "I'm a notice.",
-// });
-
-// console.log(myError);
-
-const myNotice = notice({
-  title: 'Regular Notice',
-  text: "I'm a notice.",
-  delay: 1000,
-  icon: true,
-
-  //   modules: new Map([
-  //     ...defaultModules,
-  //     [
-  //       PNotifyDesktop,
-  //       {
-  //         text: null,
-  //         // Desktop Module Options
-  //       },
-  //     ],
-  //   ]),
-});
-
-// PNotify.notice({
-//   title: 'Regular Notice',
-//   text: "Check me out! I'm a notice.",
-// });
-
-// console.log(myNotice);
+import refs from './js/refs'
+import { updateListCountry, updateCountry, clearCountry } from './js/markupCountry'
+import fetchCountries from './js/fetch-countries';
+import error from './js/alert.js';
 
 const debounce = require('lodash.debounce');
-
-console.log('Hello');
-
-const inputRef = document.querySelector('.js-input');
-const articleRef = document.querySelector('.intro');
-const wrapeRef = document.querySelector('.wrap');
-// const url = 'https://restcountries.eu/rest/v2/name/Ta';
 
 const debounsCallback = debounce(hendleInput, 1000);
 
 function hendleInput(event) {
   const value = event.target.value;
-  console.log(value);
-  fetchCountries(value).then(data => {
+  clearCountry();
+  if (!value) return;
+  fetchCountries(value)
+  .then(data => { 
+      if(data.message) {
+        return error({
+          title: data.message
+        })
+      }
     if (data.length > 10) {
-      console.log(data);
+      // console.log(data);
       return error({
-        title: 'Regular Notice',
-        text: "I'm a notice.",
-        // delay: 1000,
-        icon: true,
+        title: 'Too many matches found. Please enter a more specific query!',
       });
     }
     if (data.length >= 2) {
-      const markup = listTemplate(filterCountriesByName(data));
-      console.log(markup);
-      return (wrapeRef.innerHTML = markup);
-      //   console.log(data);
-      // filterCountriesByName(data);
+     return updateListCountry(data)
     }
-    const markupCountry = conntryTemplate(data[0]);
-    console.log(data[0]);
-    wrapeRef.innerHTML = markupCountry;
-  });
-  //   const myError = error({
-  //     text: "I'm an error message.",
-  //     delay: 500,
-  //   });
-  //   const { currentTarget } = event;
-  //   console.log(currentTarget.value);
+    // if (data.length = 1) {
+    //  console.log(data.length);
+      updateCountry(data);
+    // }
+  })
 }
 
-inputRef.addEventListener('input', debounsCallback);
-
-// console.log(fetchCountries('swit'));
-
-function filterCountriesByName(arr) {
-  return arr.map(item => item.name);
-}
-
-// const markup = listTemplate(data);
-// console.log(markup);
-// articleRef.insertAdjacentHTML('beforeend', markup);
-// console.log(data);
+refs.input.addEventListener('input', debounsCallback);
