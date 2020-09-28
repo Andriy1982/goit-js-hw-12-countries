7d689e6ca494b3cab3eb9156fba8e4e7f8597daf
Andrii Krychkivskyi <kruchandriy@gmail.com>
2020-09-28 09:18:39 +0300

    Homework-12 ready

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './styles.css';
-import refs from './js/refs'
-import { updateListCountry, updateCountry, clearCountry } from './js/markupCountry'
+import refs from './js/refs';
+import {
+  updateListCountry,
+  updateCountry,
+  clearCountry,
+} from './js/markupCountry';
 import fetchCountries from './js/fetch-countries';
 import error from './js/alert.js';
 
@@ -12,27 +16,22 @@ function hendleInput(event) {
   const value = event.target.value;
   clearCountry();
   if (!value) return;
-  fetchCountries(value)
-  .then(data => { 
-      if(data.message) {
-        return error({
-          title: data.message
-        })
-      }
+  fetchCountries(value).then(data => {
+    if (data.message) {
+      return error({
+        title: data.message,
+      });
+    }
     if (data.length > 10) {
-      // console.log(data);
       return error({
         title: 'Too many matches found. Please enter a more specific query!',
       });
     }
     if (data.length >= 2) {
-     return updateListCountry(data)
+      return updateListCountry(data);
     }
-    // if (data.length = 1) {
-    //  console.log(data.length);
-      updateCountry(data);
-    // }
-  })
+    updateCountry(data);
+  });
 }
 
 refs.input.addEventListener('input', debounsCallback);
